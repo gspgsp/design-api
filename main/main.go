2673fa1665a2d097d5c495ddf3a1809e74c8d4f5
guoshipeng <2249875980@qq.com>
2021-06-16 14:56:21 +0800

    引入包

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	_ "github.com/go-playground/validator"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("0.0.0.0:8082") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
