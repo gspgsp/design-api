394d4e916421be15b78898a00a557eb96ebd9502
guoshipeng <2249875980@qq.com>
2021-06-18 18:06:42 +0800

    添加监听地址 tcp/unix

--- main/main.go
+++ main/main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jmoiron/sqlx"
 	"design-api/config"
+	"runtime"
 )
 
 func main() {
@@ -15,7 +16,17 @@ func main() {
 	r := gin.New()
 	router.InitRouter(r)
 	fmt.Println("初始化路由...")
-	if err := r.Run(config.Config.Addr.Tcp); err != nil {
-		fmt.Println("监听出错了:" + err.Error())
+
+	sysType := runtime.GOOS
+
+	if sysType == "linux" {
+		if err := r.RunUnix(config.Config.Addr.Unix); err != nil {
+			fmt.Println("监听出错了:" + err.Error())
+		}
+	} else if sysType == "windows" {
+		if err := r.Run(config.Config.Addr.Tcp); err != nil {
+			fmt.Println("监听出错了:" + err.Error())
+		}
 	}
+
 }
