8d8976734fbffed8cd8b506580b4d0eda988a46c
guoshipeng <2249875980@qq.com>
2021-07-19 16:59:48 +0800

    登录新增mongo日志

--- handler/v1/auth/login.go
+++ handler/v1/auth/login.go
@@ -1,16 +1,16 @@
 package auth
 
 import (
-	"github.com/gin-gonic/gin"
 	"design-api/common"
-	"design-api/service"
 	"design-api/common/env"
+	mongo "design-api/common/log"
+	"design-api/service"
 	"design-api/util"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-/**
-登录
- */
+// Login /**登录
 func Login(c *gin.Context) {
 
 	code, user := service.Login(c)
@@ -19,34 +19,13 @@ func Login(c *gin.Context) {
 		common.Format(c).SetStatus(env.ERROR).SetCode(code).SetMessage(env.MsgFlags[code]).JsonResponse()
 	} else {
 		token, _ := util.GenerateToken(*user.ID)
+
+		mgo := mongo.NewMgo("login")
+		mgo.InsertOne(bson.D{{"user", user.ID}, {"token", token}})
+
 		common.Format(c).SetData(map[string]string{"token_type": "Bearer", "access_token": token}).JsonResponse()
 	}
 
-	//rows, err := common.Db.Queryx("select id, target_url, carousel_url from q_slides where status = 1 and device = 1")
-	//if err != nil {
-	//	log.Printf("查询错误:%s", err.Error())
-	//	return
-	//}
-	//
-	//slide := models.Slide{}
-	//var slides []models.Slide
-	//
-	//if rows == nil {
-	//	log.Println("数据结果为空")
-	//	return
-	//}
-	//
-	//if rows.Next() {
-	//	err := rows.StructScan(&slide)
-	//	if err != nil {
-	//		log.Printf("数据结构化错误:%s\n", err.Error())
-	//		return
-	//	} else {
-	//		slides = append(slides, slide)
-	//	}
-	//}
-	//
-	//
 	////insert redis
 	//rd := noSqlLog.NewRd(7)
 	//rd.Set("test1024", 1025, -1)
