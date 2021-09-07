98407cfc07812f05f02a0ba6a0609e69c6f121c0
guoshipeng <2249875980@qq.com>
2021-09-07 16:25:33 +0800

    json参数调试

--- handler/v1/common/sms.go
+++ handler/v1/common/sms.go
@@ -1,12 +1,10 @@
 package common
 
 import (
-	//_ "github.com/patrickmn/go-cache"
-	"design-api/service"
+	"encoding/json"
+	"log"
+
 	"github.com/gin-gonic/gin"
-	"design-api/common/env"
-	"design-api/validator/auth"
-	"design-api/common"
 )
 
 //验证码长度
@@ -16,19 +14,27 @@ var codeLength = 6
 发送短信验证码
  */
 func SendSms(c *gin.Context) {
-	mobile, _ := c.GetPostForm("mobile")
-	smsParam := &auth.SmsParam{Mobile: mobile}
-	if code, _ := smsParam.ValidateParam(); code == env.RESPONSE_SUCCESS {
-		sms := &service.SmsService{Len: codeLength}
-		codeKey, err := sms.SendSmsCode(mobile)
-		if err != nil {
-			common.Format(c).SetStatus(env.ERROR).SetCode(env.SMS_CODE_SEND_ERROR).SetMessage(env.MsgFlags[env.SMS_CODE_SEND_ERROR]).JsonResponse()
-			c.Abort()
-			return
-		}
 
-		common.Format(c).SetData(map[string]interface{}{"code_key": codeKey}).JsonResponse()
-	} else {
-		common.Format(c).SetStatus(env.ERROR).SetCode(env.PARAM_REQUIRED).SetMessage(env.MsgFlags[env.PARAM_REQUIRED]).JsonResponse()
-	}
+	b, _ := c.GetRawData()
+	var m map[string]interface{}
+	_ = json.Unmarshal(b,&m)
+	log.Printf("m is:%v", m)
+	return
+
+
+	//mobile, _ := c.GetPostForm("mobile")
+	//smsParam := &auth.SmsParam{Mobile: mobile}
+	//if code, _ := smsParam.ValidateParam(); code == env.RESPONSE_SUCCESS {
+	//	sms := &service.SmsService{Len: codeLength}
+	//	codeKey, err := sms.SendSmsCode(mobile)
+	//	if err != nil {
+	//		common.Format(c).SetStatus(env.ERROR).SetCode(env.SMS_CODE_SEND_ERROR).SetMessage(env.MsgFlags[env.SMS_CODE_SEND_ERROR]).JsonResponse()
+	//		c.Abort()
+	//		return
+	//	}
+	//
+	//	common.Format(c).SetData(map[string]interface{}{"code_key": codeKey}).JsonResponse()
+	//} else {
+	//	common.Format(c).SetStatus(env.ERROR).SetCode(env.PARAM_REQUIRED).SetMessage(env.MsgFlags[env.PARAM_REQUIRED]).JsonResponse()
+	//}
 }
